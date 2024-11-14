e2f582938f5959ef16cc2ff9075aa2bbe7f96e5f
Ilya Sergeev <54859823+IlyaS-26@users.noreply.github.com>
2024-11-14 04:35:57 +0300

    Delete Logic_AND.sv

--- Logic_AND.sv
+++
@@ -1,7 +0,0 @@
-module and_gate (
-    input logic a,    // Вход a
-    input logic b,    // Вход b
-    output logic y    // Выход y
-);
-    assign y = a & b; // Логическая операция AND
-endmodule
