0035244687cc5c3ea57dee84220a4daf42daefbb
Ilya Sergeev <54859823+IlyaS-26@users.noreply.github.com>
2024-11-14 04:32:44 +0300

    Add files via upload

---
+++ Logic_NOT.sv
@@ -0,0 +1,6 @@
+module not_gate (
+    input logic a,    // Вход a
+    output logic y    // Выход y
+);
+    assign y = ~a;    // Логическая операция NOT
+endmodule
---
+++ Logic_OR.sv
@@ -0,0 +1,7 @@
+module or_gate (
+    input logic a,    // Вход a
+    input logic b,    // Вход b
+    output logic y    // Выход y
+);
+    assign y = a | b; // Логическая операция OR
+endmodule
